ba908f846f67bf29f72c50b03cf0726593b481a9
Giovanni-dotcome <giodevito99@gmail.com>
2021-04-29 13:59:16 +0200

    milestone 2

--- js/script.js
+++ js/script.js
@@ -2,15 +2,15 @@ var app = new Vue({
     el: '#root',
     data: {
         searched:'',
-        movies: []
+        movies: [],
+        test: 'us'
     },
     methods: {
         searchMovie() {
-            axios.get(`https://api.themoviedb.org/3/search/movie/?api_key=2d61f612414428cd866f192ad6c518ae&query=${this.searched}`).then(response => {
+            axios.get(`https://api.themoviedb.org/3/search/multi?api_key=2d61f612414428cd866f192ad6c518ae&query=${this.searched}`).then(response => {
                 this.movies = response.data.results;
-                console.log(this.movies);
+                this.searched = '';
             })
-            this.searched = '';
         }
     }
 })
