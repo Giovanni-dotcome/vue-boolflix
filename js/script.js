b08773202a7764c24ab2ffcb24fac8536c459cc6
Giovanni-dotcome <giodevito99@gmail.com>
2021-04-29 12:36:15 +0200

    prima milestone

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 var app = new Vue({
     el: '#root',
     data: {
-
+        searched:'',
+        movies: []
     },
     methods: {
-        
+        searchMovie() {
+            axios.get(`https://api.themoviedb.org/3/search/movie/?api_key=2d61f612414428cd866f192ad6c518ae&query=${this.searched}`).then(response => {
+                this.movies = response.data.results;
+                console.log(this.movies);
+            })
+            this.searched = '';
+        }
     }
 })
